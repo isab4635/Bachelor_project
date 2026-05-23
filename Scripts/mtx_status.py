85334d93dec9a0560826d348869511dffa892b7c
isab4635 <isabella.t.chr@gmail.com>
2026-05-23 07:34:16 +0200

    Latest visit must be 6 months after start and now it is called mtx_success instead of mtx_fail

--- Scripts/mtx_status.py
+++ Scripts/mtx_status.py
@@ -1,6 +1,6 @@
 #!/usr/local/anaconda3-2024.10-1/bin/python3
 # Edited: 14/4/2026
-# The purpose of this script is to get a file with patient_id and mtx_fail (0: fail, 1: success)
+# The purpose of this script is to get a file with patient_id and mtx_success (0: fail, 1: success)
 # This is done on the basis of a timeline style file (normalized to mtx and with diagnosis date)
 
 
@@ -16,17 +16,16 @@ df['Date'] = pd.to_datetime(df['Date'])
 df['diagnosis_date'] = pd.to_datetime(df['diagnosis_date'])
 df['first_mtx_date'] = pd.to_datetime(df['first_mtx_date'])
 
-# ---- Select what patients we want to look at when determining mtx_fail ---- #
-
+# ---- Select what patients we want to look at when determining mtx_success ---- #
 # Print patients at start
 count = df["patient_id"].nunique()
 print("Number of patients before anything: ")
 print(count)
 
-### Ensure that patients do have a check-in visit some time after mtx_start ###
+### Ensure that patients do have a check-in visit 6 months after mtx_start ###
 # Find max visit date. (Inspired by https://stackoverflow.com/questions/23394476/keep-other-columns-when-doing-groupby)
 max_visit_dates = df.loc[df.groupby("patient_id")["Date"].idxmax()]
-patients_no_follow_up = max_visit_dates[max_visit_dates["Date"] <= max_visit_dates["first_mtx_date"]]
+patients_no_follow_up = max_visit_dates[(max_visit_dates["Date"] - max_visit_dates["first_mtx_date"]) <= pd.Timedelta(days = 30.44*6)]
 
 patients_no_follow_up.to_csv("../data/filtered_mtx_fail/patients_no_follow_up.csv")
 df = df[~df["patient_id"].isin(patients_no_follow_up["patient_id"])]
@@ -55,8 +54,8 @@ print("Number of patients with less than 6 months between diag and mtx: ")
 print(count)
 
 
-# ---- Determine if MTX fail in first year --- #
-# Failure: Prescription stop of mtx lasts more than 3 months. Value of 0 in mtx_fail
+# ---- Determine if MTX discontinued in first year --- #
+# Failure: Prescription stop of mtx lasts more than 3 months. Value of 0 in mtx_success
 # Limit to only first year (12+3 months to test if they get back on mtx)
 df = df[(df['months_since_mtx'] <= 15) & (df['months_since_mtx'] >= 0)]
 
@@ -70,9 +69,9 @@ mtx_events["Next_date"] = mtx_events.groupby("patient_id")["Date"].shift(-1)
 # Add column with time between events (gap)
 mtx_events["Gap"] = mtx_events["Next_date"] - mtx_events["Date"]
 
-# Initialize dataframe with one row per patient to collect mtx_failures
+# Initialize dataframe with one row per patient to collect mtx_success
 unique_ids = df["patient_id"].unique()
-mtx_status = pd.DataFrame({"patient_id": unique_ids, "mtx_fail": None})
+mtx_status = pd.DataFrame({"patient_id": unique_ids, "mtx_success": None})
 
 # Go over each mtx_stop event
 for stops in mtx_events[mtx_events["Event"] == "Prescription_stop"].itertuples():
@@ -81,16 +80,16 @@ for stops in mtx_events[mtx_events["Event"] == "Prescription_stop"].itertuples()
         continue
     # Else this stop indicates an mtx failure and is saved as such
     else:
-        mtx_status.loc[mtx_status["patient_id"] == stops.patient_id, "mtx_fail"] = 0
+        mtx_status.loc[mtx_status["patient_id"] == stops.patient_id, "mtx_success"] = 0
 
-# Every value in mtx_fail which is still none is a success and set to 1
-mtx_status['mtx_fail'] = mtx_status['mtx_fail'].fillna(1).astype(int)
+# Every value in mtx_success which is still none is a success and set to 1
+mtx_status['mtx_success'] = mtx_status['mtx_success'].fillna(1).astype(int)
 
 
 # ---- Save file ---- #
 # Print info about it
 print(f"Total patients: {len(mtx_status.index)}")
-nan_included_counts = mtx_status['mtx_fail'].value_counts(dropna=False)
+nan_included_counts = mtx_status['mtx_success'].value_counts(dropna=False)
 print(nan_included_counts)
 
 mtx_status.to_csv("../data/mtx_status.csv")
